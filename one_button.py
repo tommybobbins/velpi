6d58942ad5cbf92a0779cbdc876202a4312d82d1
Tim Gibbon <tng@chegwin.org>
2014-04-24 21:52:08 +0000

    Added one_button.py script, cleaned up existing relay test script

--- one_button.py
+++ one_button.py
@@ -2,6 +2,8 @@
 
 import smbus
 import time 
+import sys
+
 
 bus = smbus.SMBus(1)  #for revision 2 board...use 1
 
@@ -13,6 +15,7 @@ bank0 = 0 #Contains just power for relay board
 bank1 = 1 #Contains all relays
 bus.write_byte_data(address,0x00,0x00) # BankA outputs 
 bus.write_byte_data(address,0x01,0x00) # BankB outputs
+
 
 relay0 = 0
 relay1 = 1
@@ -27,7 +30,7 @@ relay = [0,1,2,4,8,16,32,64,128]
 
 def set_relay(data,bank):
 
-  print "set_relay bank=" + str(bank) + " data=" + str(data) 
+#  print "set_relay bank=" + str(bank) + " data=" + str(data) 
   if bank == 1:
      data = int(255 - data) # We only want to switch on one relay at a time
      bus.write_byte_data(address,0x12,data)
@@ -36,22 +39,23 @@ def set_relay(data,bank):
   else:
      print ("Something has gone horribly wrong")
      exit()
-  time.sleep(1)
+  time.sleep(0.3)
   return
   
-def main():
+def press_button(thisrelay):
    set_relay(relay[0],bank0) #switch off relay board
-   set_relay(relay[0],bank1) #switch off relays
-   print "Switching on relay1, bank0"
+   set_relay(relay[0],bank1) #switch off relay board
    set_relay(1,bank0) #switch on relay board
-   counter = 0;
-   while counter<9:
-       this_relay=int(relay[counter])
-       set_relay(this_relay,bank1)
-       set_relay(relay0,bank1) #clear relays
-       counter += 1
-   print ("Cleaning up")
+   this_relay=int(relay[thisrelay])
+   set_relay(this_relay,bank1)
+   set_relay(relay[0],bank1) #clear Relay
    set_relay(relay[0],bank0) #switch off relay board
  
 if __name__ == "__main__":
-   main()
+    try:
+        relay_number=int(sys.argv[1])
+    except:
+        print ("You need to supply the relay number e.g %s %i" % (sys.argv[0], 3))
+        exit()
+#    print ("We want to press %i " % relay_number)
+    press_button(relay_number)
