2fa2b478e6dbc31ad28d9182c4cfffb8d2693d7e
Tim Gibbon <tng@chegwin.org>
2014-07-11 17:20:32 +0100

    Modified Half open to reflect status

--- all_half.sh
+++ all_half.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-/usr/bin/redis-cli -h 433host set "velux/3" "Half"
-/usr/bin/redis-cli -h 433host set "velux/2" "Half"
-/usr/bin/redis-cli -h 433host set "velux/1" "Half"
-echo "All open" >/tmp/fullopen.txt
+echo "All Half" >/tmp/fullopen.txt
 /usr/local/bin/one_button.py 3
 sleep 1
 /usr/local/bin/one_button.py 4
@@ -24,4 +21,7 @@ sleep 1
 /usr/local/bin/one_button.py 4
 sleep 1
 /usr/local/bin/one_button.py 1
+/usr/bin/redis-cli -h 433host set "velux/3" "Half"
+/usr/bin/redis-cli -h 433host set "velux/2" "Half"
+/usr/bin/redis-cli -h 433host set "velux/1" "Half"
 /usr/bin/redis-cli -h 433host expire "velux/3" 14400
